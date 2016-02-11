18d0795e355d337c44edf979e4852a6fec9d78d6
= <cireL92@gmail.com>
2016-02-11 15:37:52 -0800

    Implemented baz for Q4.
    Note: def self.baz creates a class method, whereas def baz creates an instance method.

--- hw1/foobar.rb
+++ hw1/foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(arr)
+    new_arr = arr.map{ |x| x = x.to_f + 2}
+    new_arr.delete_if{ |x| x % 2 != 0 }
+    new_arr.uniq!
+    new_arr.delete_if{ |x| x > 10}
+    new_arr.inject{|sum, x| sum + x}
+  end
 end
