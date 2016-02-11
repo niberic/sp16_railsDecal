dcb86b03913d445012e310d82267b711002e428a
= <cireL92@gmail.com>
2016-02-11 11:53:48 -0800

    Finished q1 and q2. Note: collect == map. Ruby is weird.

--- hw1/hw1.rb
+++ hw1/hw1.rb
@@ -1,9 +1,11 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.sort.map { |x| x += 1 } 
 end
 
 def combine_name(first_name, last_name)
